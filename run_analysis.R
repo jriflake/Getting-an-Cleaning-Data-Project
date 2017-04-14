03e4c384f13ea7d92ebfaf22660548702d2746bb
jriflake <jriflake@gmail.com>
2017-04-14 16:03:09 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,21 +2,33 @@ run_analysis <- function(){
   library(data.table)
   library(dplyr)
   library(tidyr)
-  
+  ## Merge results
  Tot <-merge(train,test,all = TRUE) 
+ ## add column names
  colnames(Tot)<- tnames$V2
+ ## filter to mean and std columns
  MSTot <- Tot[,grepl("mean()|std()",names(Tot))]
  MSTot2 <- MSTot[,!grepl("meanFreq" , names(MSTot))]
+ ## merge activity files
  tactive <- rbind(activities,teactivities)
+ ## merge subject files
  subjects <- rbind(subtrain,subtest)
+ ## add column name to subject file
  colnames(subjects) <- "subject"
+ ## associate activity names with the numbers
  activenames <- merge(tactive,act_labels,by = "V1")
  colnames(activenames)<- c("Act_number","Activity_Name")
+ ## add activity and subject columns to results data
  MSTot3 <- cbind(MSTot2,activenames$Activity_Name)
  MSTot4 <- cbind(subjects$subject,MSTot3)
  names(MSTot4)[1] <- "subject"
  names(MSTot4)[68] <- "activity"
+ ## create average data set by subject and activity
  MSTot5 <- MSTot4 %>% group_by(activity,subject) %>% ddply(.(activity,subject),numcolwise(mean)) %>% arrange(activity,desc(subject))
+ ## clean up column names
+ colnames(MSTot5)<- gsub("-","",names(MSTot5))
+ colnames(MSTot5)<- gsub("[()]","",names(MSTot5))
  MSTot5
+ 
  
 }
